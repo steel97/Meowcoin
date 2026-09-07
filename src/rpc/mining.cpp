6d7a0c1664b134d77f91e02c5def21ec706e68ba
Ivan Yv <ivan.yurkov@steel-team.net>
2026-09-07 22:29:35 +0700

    fix fix fuck fix

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -692,7 +692,7 @@ static RPCHelpMan getblocktemplate()
         "    https://github.com/meowcoin/bips/blob/master/bip-0009.mediawiki#getblocktemplate_changes\n"
         "    https://github.com/meowcoin/bips/blob/master/bip-0145.mediawiki\n",
         {
-            {"template_request", RPCArg::Type::OBJ, RPCArg::Optional::NO, "Format of the template",
+            {"template_request", RPCArg::Type::OBJ, RPCArg::Optional::OMITTED, "Format of the template",
             {
                 {"mode", RPCArg::Type::STR, /* treat as named arg */ RPCArg::Optional::OMITTED, "This must be set to \"template\", \"proposal\" (see BIP 23), or omitted"},
                 {"capabilities", RPCArg::Type::ARR, /* treat as named arg */ RPCArg::Optional::OMITTED, "A list of strings",
